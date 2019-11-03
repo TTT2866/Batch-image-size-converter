af9cfb30ffa965d6008806ee1b59b4c8a36dddd2
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2019-11-03 16:40:36 +1100

    Update convert.bat

--- convert.bat
+++ convert.bat
@@ -3,9 +3,16 @@ title Converter
 color f
 mode 40,10
 echo %1
+echo File size is %~z1 kb
+
+
 set filenamenoex=%~n1
 set filename=%1
-echo.
+set number=%random%
 echo Converting file...
-magick convert %filename% -resize 4000 %filenamenoex%%random%.jpg
+magick convert %filename% -resize 4000 %filenamenoex%%number%.jpg
+echo Done.
+FOR /F "usebackq" %%A IN ('%filenamenoex%%number%.jpg') DO set size=%%~zA 
+echo New file size is %size% kb
+pause
 exit
