8073f7e2a80400a8cb3197b27e6c3245c85a9c43
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2019-11-07 21:24:31 +1100

    Update convert.bat
    It saves a version of the photo converted in your Appdata\ConvertCache in case you don't have your SD card or a backup and need the photo

--- convert.bat
+++ convert.bat
@@ -2,17 +2,20 @@
 title Converter
 color f
 mode 40,10
+nircmd wait 6000
 echo %1
 echo File size is %~z1 kb
 
-
+set d1=%date: =_% & set d2=%d1:/=_%
 set filenamenoex=%~n1
 set filename=%1
 set number=%random%
+if not exist %appdata%\ConvertCache\%d2% MD %appdata%\ConvertCache\%d2%
 echo Converting file...
 magick convert %filename% -resize 4000 %filenamenoex%%number%.jpg
 echo Done.
 FOR /F "usebackq" %%A IN ('%filenamenoex%%number%.jpg') DO set size=%%~zA 
+if exist %filenamenoex%%number%.jpg (copy %filenamenoex%%number%.jpg %appdata%\ConvertCache\%d2% >nul) else (echo error.. dont know what to do lol)
 echo New file size is %size% kb
 pause
 exit
